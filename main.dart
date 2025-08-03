68792ebfff4cc15001140a400bfff617c182a708
editorxpro <sushantxsahu123@gmail.com>
2025-08-03 12:08:13 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,2 +1,10 @@
-// Your EditProX full code goes here.
-void main() => print('EditProX Ready');
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text("EditProX")),
+      body: Center(child: Text("Welcome to EditProX App")),
+    ),
+  ));
+}
